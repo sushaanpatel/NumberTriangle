7f394122c74d336df83eb2be638f52caeb7a68cd
Sushaan Patel <sushaanpatel@gmail.com>
2025-09-22 19:48:42 -0400

    TODO removed

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -95,7 +95,6 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
         if (path.isEmpty()) {
             return this.getRoot();
         } else {
